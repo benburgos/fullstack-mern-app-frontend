4db7351db7c0f174a976a1bb235cd0d6458ad0a2
Ben <ben.a.burgos@gmail.com>
2022-05-25 22:08:52 -0700

    completes initial Show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,5 +1,17 @@
+import { useParams } from 'react-router-dom';
+
 function Show(props) {
-  return <h1>Show</h1>;
+  const { id } = useParams();
+  const people = props.people;
+  const person = people.find((p) => p._id === id);
+
+  return (
+    <div className="person">
+      <h1>{person.name}</h1>
+      <h2>{person.title}</h2>
+      <img src={person.image} alt={person.name} />
+    </div>
+  );
 }
 
 export default Show;
