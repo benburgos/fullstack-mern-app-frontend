363e7b00a69d1ee927ccca8dbae7b1b8c65f49a1
Ben <ben.a.burgos@gmail.com>
2022-05-25 19:31:05 -0700

    removes broken code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,39 +1,12 @@
-import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Index from '../pages/Index';
 import Show from '../pages/Show';
 
 function Main(props) {
-  const [people, setPeople] = useState(null);
-
-  const URL = 'https://bb-fullstack-mern-app-backend.herokuapp.com/people';
-
-  const getPeople = async () => {
-    const response = await fetch(URL);
-    const data = await response.json();
-    setPeople(data);
-  };
-
-  const createPeople = async (person) => {
-    await fetch(URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'Application/json',
-      },
-      body: JSON.stringify(person),
-    });
-    getPeople();
-  };
-
-  useEffect(() => getPeople(), []);
-
   return (
     <main>
       <Routes>
-        <Route
-          path="/"
-          element={<Index people={people} createPeople={createPeople} />}
-        />
+        <Route exact path="/" element={<Index />} />
         <Route path="/people/:id" element={<Show />} />
       </Routes>
     </main>
