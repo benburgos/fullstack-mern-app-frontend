da327bffa528ee98502da79f6af16c341ea7b159
Ben <ben.a.burgos@gmail.com>
2022-05-25 20:34:54 -0700

    completes index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,24 @@
-function Index(props) {
-  return <h1>Index</h1>;
+import { Link } from 'react-router-dom';
+
+function Index({ people, createPeople }) {
+  // Loaded Function
+  const loaded = () => {
+    return people.map((person) => (
+      <div key={person._id} className="person">
+        <Link to={`/people/${person._id}`}>
+          <h1>{person.name}</h1>
+        </Link>
+        <img src={person.image} alt={person.name} />
+        <h3>{person.title}</h3>
+      </div>
+    ));
+  };
+  // Loading Function
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  return people ? loaded() : loading();
 }
 
 export default Index;
